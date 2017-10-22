a7616db61640fbf50adb35baa0220a1f394228c8
peter0123chang <peter0123chang@gmail.com>
2017-10-22 14:10:23 +0800

    Update echo-express.js

--- examples/echo-express.js
+++ examples/echo-express.js
@@ -1,7 +1,7 @@
-const linebot = require('../index.js');
+const linefoodbot = require('../index.js');
 const express = require('express');
 
-const bot = linebot({
+const bot = linefoodbot({
 	channelId: process.env.CHANNEL_ID,
 	ChannelSecret: process.env.CHANNEL_SECRET,
 	ChannelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
@@ -12,7 +12,21 @@ const app = express();
 const linebotParser = bot.parser();
 
 app.post('/linewebhook', linebotParser);
+/*
+bot.on('message', function(event) {
+  console.log(event); //把收到訊息的 event 印出來看看
+});
 
+const app = express();
+const linebotParser = bot.parser();
+app.post('/', linebotParser);
+
+//因為 express 預設走 port 3000，而 heroku 上預設卻不是，要透過下列程式轉換
+var server = app.listen(process.env.PORT || 8080, function() {
+  var port = server.address().port;
+  console.log("App now running on port", port);
+});
+*/
 bot.on('message', function (event) {
 	event.reply(event.message.text).then(function (data) {
 		console.log('Success', data);
