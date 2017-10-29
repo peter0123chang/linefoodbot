e1c12ac9816cd80c66a302ea80d4ee9c2f5680c0
peter0123chang <peter0123chang@gmail.com>
2017-10-29 13:21:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+var linebot = require('linebot');
  // var CHANNEL_ID= '1541948237';
 //  var CHANNEL_SECRET= '9a8c7887c8ed7ceb3f963ce8f73d64a9';
 //  var CHANNEL_ACCESS_TOKEN= '3cL4zroJybMhWDUTZPUnLbxzPjfsoNu5FO0EqmZ/fhOka8bgzPZBCCp2EkmVw4l5lRUhDRML3vSuT9wV+VUd/xzw02X6FL6nFi8LaaT8NDuJcNJaaIIyNDzB/C5dfeU21BS73yzS3jQ88fmlwQS1VAdB04t89/1O/w1cDnyilFU=';
