4439d1f382189c4ba2cd97b6dc4311a35bb9959c
peter0123chang <peter0123chang@gmail.com>
2017-10-29 12:55:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
   var CHANNEL_ACCESS_TOKEN= '3cL4zroJybMhWDUTZPUnLbxzPjfsoNu5FO0EqmZ/fhOka8bgzPZBCCp2EkmVw4l5lRUhDRML3vSuT9wV+VUd/xzw02X6FL6nFi8LaaT8NDuJcNJaaIIyNDzB/C5dfeU21BS73yzS3jQ88fmlwQS1VAdB04t89/1O/w1cDnyilFU=';
 var bot = linebot({
 	channelId: CHANNEL_ID,
-	channelSecret: CHANNEL_SECRET,
-	channelAccessToken: CHANNEL_ACCESS_TOKEN,
+	ChannelSecret: CHANNEL_SECRET,
+	ChannelAccessToken: CHANNEL_ACCESS_TOKEN,
 	verify: true // Verify 'X-Line-Signature' header (default=true)
 });
